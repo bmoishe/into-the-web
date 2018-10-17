38b31b9d7f249b3e1e3c51dba7d159c17b230622
bmoishe <moishebarnes@gmail.com>
2018-10-17 14:24:28 +0100

    Second commit

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
+#require 'shotgun'
 
 get '/' do
-  'hello!'
+  'hello!1'
 end
 
 
 get '/secret' do
-  'This is a secrect page'
+  'This is a secrect page1'
+end
+
+get '/cat' do
+  "<div>
+  <img scr='http://bit.ly/1eze8aE'>
+  </div>"
 end
